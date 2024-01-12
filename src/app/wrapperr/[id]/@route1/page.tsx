81233a88ebcf063e0afd5f73b5d648308ee09d70
SeungYn <kwls0407@gmail.com>
2024-01-13 00:49:08 +0900

    bug: nextjs parallel generateMetadata

---
+++ src/app/wrapperr/[id]/@route1/page.tsx
@@ -0,0 +1,3 @@
+export default function page() {
+  return <div>라우터1</div>;
+}
---
+++ src/app/wrapperr/[id]/layout.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react';
+
+export function generateMatadata({
+  params: { id },
+}: {
+  params: { id: number };
+}) {
+  return { title: 'test' };
+}
+
+export default function Layout({
+  children,
+  route1,
+  params: { id },
+}: {
+  children: React.ReactNode;
+  route1: ReactNode;
+  params: { id: number };
+}) {
+  return (
+    <div>
+      {/* {children}
+      {route1} */}
+    </div>
+  );
+}
